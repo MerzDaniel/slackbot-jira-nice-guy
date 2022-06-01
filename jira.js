08e599ccbf80f77d1bd731e892d3e2d0f63a856f
Daniel Merz <Daniel.Merz@Actano.de>
2022-06-01 10:52:36 +0200

    Initialize jira only when needed

--- jira.js
+++ jira.js
@@ -3,29 +3,35 @@
 // https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-projects/
 // https://docs.atlassian.com/jira-software/REST/7.0.4/#agile/1.0/board/{boardId}/sprint-getAllSprints
 
-import jiraClient from "jira-connector";
+import JiraClient from "jira-connector";
 
-var jira = new jiraClient({
-  host: process.env.jiraHost,
-  basic_auth: {
-    email: process.env.jiraUser,
-    api_token: process.env.jiraToken
+let jiraClient
+function getJira() {
+  if (!jiraClient) {
+    jiraClient = new JiraClient({
+      host: process.env.jiraHost,
+      basic_auth: {
+        email: process.env.jiraUser,
+        api_token: process.env.jiraToken
+      }
+    })
   }
-})
+  return jiraClient
+}
 
 export async function getAllBoards() {
-  console.log(await jira.board.getAllBoards())
+  console.log(await getJira().board.getAllBoards())
 }
 export async function getIssue(issueId) {
   const fields = 'status,issuetype,summary'
-  console.log(await jira.issue.getIssue({ issueId }))
+  console.log(await getJira().issue.getIssue({ issueId }))
 }
 export async function getAllSprints() {
-  console.log((await jira.board.getSprintsForBoard({ boardId: 1, startAt: 100 })).values)
+  console.log((await getJira().board.getSprintsForBoard({ boardId: 1, startAt: 100 })).values)
 }
 export async function getAllSprintIssues(id) {
   const fields = 'status,issuetype,summary'
-  const { issues } = await jira.sprint.getSprintIssues({
+  const { issues } = await getJira().sprint.getSprintIssues({
     sprintId: id,
     fields,
     maxResults: 1000,
